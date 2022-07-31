04b3be59d6c4136121d8cff73e35e198a9e96d58
ayaan-sayyed <84319719+ayaan-sayyed@users.noreply.github.com>
2022-07-31 16:49:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-TheNights = "";
-GamesBegin = "";
-MagentaRiddim = "";
+ var TheNights = "";
+ var GamesBegin = "";
+ var MagentaRiddim = "";
 
 function preload() {
     TheNights = loadSound("The Nights.mp3");
-    GamesBegin = loadSound("LET THE GAMES BEGIN.m3");
+   GamesBegin = loadSound("LET THE GAMES BEGIN.mp3");
     MagentaRiddim = loadSound("Magenta Riddim.mp3");
 }
 
 function setup() {
     canvas = createCanvas(600, 400);
     canvas.position(480, 250);
-    background("white");
     video = createCapture(VIDEO);
     video.hide();
 }
@@ -24,5 +23,9 @@ function draw() {
 function play() {
     TheNights.play();
     TheNights.setVolume(0.7);
-    TheNights.rate(2.5);
+    TheNights.rate(1);
+}
+
+function stop() {
+    TheNights.stop();
 }
